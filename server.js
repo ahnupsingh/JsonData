6b79444cb27191d074f79092a874d94f213e7e5b
ahnupsingh <anup.singh2071@gmail.com>
2019-07-10 21:03:17 +0545

    Add Paxsafe Json Response

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const router2 = jsonServer.router('posts.json');
+const paxsafeRouter = jsonServer.router('paxsafe.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 server.use(router2);
+server.use(paxsafeRouter);
 
 server.listen(port);
